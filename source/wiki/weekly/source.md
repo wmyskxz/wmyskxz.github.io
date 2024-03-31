7e97f5dd907c3d565106f1ca9e6b829b0b8977d5
wmyskxz <wmyskxz@gmail.com>
2024-04-01 01:10:25 +0800

    Site updated: 2024-04-01 01:10:22

---
+++ source/wiki/weekly/source.md
@@ -0,0 +1,18 @@
+---
+wiki: weekly
+title: 周刊来源
+---
+
+这里记录了周刊的部分资讯来源。欢迎评论区（或者微信）补充交流。
+
+{% quot 优质资讯网站 icon:hashtag %}
+
+{% sites sites_news %}
+
+{% quot NewsLetter icon:hashtag %}
+
+{% sites sites_newsletter %}
+
+{% quot 博客网站 icon:hashtag %}
+
+{% sites sites_blog %}
--- trasnfer/runMe.py
+++ trasnfer/runMe.py
@@ -1,10 +1,8 @@
-import re
 import os
-import sys
+import re
+import socket
 import time
 from urllib.request import urlretrieve
-import socket
-import json
 
 socket.setdefaulttimeout(30)  # 设置默认超时时间是 30s
 
@@ -33,82 +31,82 @@ exp = r'(?:!\[(.*?)\]\((.*?)\))'
 
 
 def CreatePicDirAndRetFinalPath(path):
-  path = str(path)
-  if path.startswith('https://cdn.jsdelivr.net/gh/wmyskxz'):
-    # eg: https://cdn.jsdelivr.net/gh/wmyskxz/BlogImage02/2021-2-21/1613872075026-image.png
-    return ''
-    # dirAndName = path.split("https://cdn.jsdelivr.net/gh/wmyskxz/")[-1].split("/")
-    # dirAndName 这里出来是一个列表：['BlogImage02', '2021-2-21', '1613872075026-image.png']
+    path = str(path)
+    if path.startswith('https://cdn.jsdelivr.net/gh/wmyskxz'):
+        # eg: https://cdn.jsdelivr.net/gh/wmyskxz/BlogImage02/2021-2-21/1613872075026-image.png
+        return ''
+        # dirAndName = path.split("https://cdn.jsdelivr.net/gh/wmyskxz/")[-1].split("/")
+        # dirAndName 这里出来是一个列表：['BlogImage02', '2021-2-21', '1613872075026-image.png']
 
-  if path.startswith("https://gitee.com/wmyskxz/pic/raw/master"):
-    # eg: https://gitee.com/wmyskxz/pic/raw/master/20220306210249.png
+    if path.startswith("https://gitee.com/wmyskxz/pic/raw/master"):
+        # eg: https://gitee.com/wmyskxz/pic/raw/master/20220306210249.png
 
-    name = path.split("/")[-1]
-    dirAndName = ['pic', 'raw', 'master', name]
+        name = path.split("/")[-1]
+        dirAndName = ['pic', 'raw', 'master', name]
 
-  elif path.startswith('https://files.mdnice.com/user/4774'):
-    # eg: https://files.mdnice.com/user/4774/be44f336-e8cd-4c26-9a0a-c5047979e340.png
+    elif path.startswith('https://files.mdnice.com/user/4774'):
+        # eg: https://files.mdnice.com/user/4774/be44f336-e8cd-4c26-9a0a-c5047979e340.png
 
-    name = path.split("/")[-1]
-    dirAndName = ['gitee', name]
-  else:
-    print('!---------失败 path:', path)
-    return ''
+        name = path.split("/")[-1]
+        dirAndName = ['gitee', name]
+    else:
+        print('!---------失败 path:', path)
+        return ''
 
-  tempPath = ''
-  try:
-    for dirOrName in dirAndName:
-      tempPath += dirOrName
-      if dirOrName != dirAndName[-1]:
-        # 但凡不是最后一个文件名，那么都是文件夹，没有的话需要提前创建一个
-        if not os.path.exists(tempPath):
-          os.mkdir(tempPath)
-      else:
-        # 如果是文件名了，则直接退出
-        break
-      tempPath += '/'
-  except:
-    print('!---------失败dirAndName:', dirAndName)
+    tempPath = ''
+    try:
+        for dirOrName in dirAndName:
+            tempPath += dirOrName
+            if dirOrName != dirAndName[-1]:
+                # 但凡不是最后一个文件名，那么都是文件夹，没有的话需要提前创建一个
+                if not os.path.exists(tempPath):
+                    os.mkdir(tempPath)
+            else:
+                # 如果是文件名了，则直接退出
+                break
+            tempPath += '/'
+    except:
+        print('!---------失败dirAndName:', dirAndName)
 
-  return tempPath
+    return tempPath
 
 
 if __name__ == '__main__':
 
-  fileList = filter(lambda temp: str(temp).endswith(".md"), os.listdir("."))
-  fileList = sorted(fileList)  # 排序
+    fileList = filter(lambda temp: str(temp).endswith(".md"), os.listdir("."))
+    fileList = sorted(fileList)  # 排序
 
-  for fileName in fileList:
-    start = time.time()
-    with open(fileName, "r", encoding='utf-8') as read_f:
-      fileLines = read_f.read()
+    for fileName in fileList:
+        start = time.time()
+        with open(fileName, "r", encoding='utf-8') as read_f:
+            fileLines = read_f.read()
 
-      imageArr = re.findall(exp, fileLines)
+            imageArr = re.findall(exp, fileLines)
 
-      count = 0
-      for desc, path in imageArr:
-        count += 1
-        finalPath = CreatePicDirAndRetFinalPath(path)
-        if not finalPath:
-          if not path.startswith("https://cdn.jsdelivr.net/gh/wmyskxz"):
-            print('!!!! 失败：', path)
-          continue
-        try:
-          urlretrieve(path, finalPath)
-        except TimeoutError:
-          try:
-            # 重试一次
-            urlretrieve(path, finalPath)
-          except:
-            print('----错误：', path)
-        percent = round(float(count) / len(imageArr), 2) * 100
-        print('\r', '---------正在处理： %s [%d/%d]' % (fileName, count, len(imageArr)), end='')
+            count = 0
+            for desc, path in imageArr:
+                count += 1
+                finalPath = CreatePicDirAndRetFinalPath(path)
+                if not finalPath:
+                    if not path.startswith("https://cdn.jsdelivr.net/gh/wmyskxz"):
+                        print('!!!! 失败：', path)
+                    continue
+                try:
+                    urlretrieve(path, finalPath)
+                except TimeoutError:
+                    try:
+                        # 重试一次
+                        urlretrieve(path, finalPath)
+                    except:
+                        print('----错误：', path)
+                percent = round(float(count) / len(imageArr), 2) * 100
+                print('\r', '---------正在处理： %s [%d/%d]' % (fileName, count, len(imageArr)), end='')
 
-    fileLines = fileLines.replace("https://cdn.jsdelivr.net/gh/wmyskxz", 'https://wmyskxz-blog.oss-cn-chengdu.aliyuncs.com') \
-      .replace("https://gitee.com/wmyskxz/pic/raw/master", 'https://wmyskxz-blog.oss-cn-chengdu.aliyuncs.com/pic/raw/master') \
-      .replace("https://files.mdnice.com/user/4774", 'https://wmyskxz-blog.oss-cn-chengdu.aliyuncs.com/gitee')
+        fileLines = fileLines.replace("https://cdn.jsdelivr.net/gh/wmyskxz", 'https://wmyskxz-blog.oss-cn-chengdu.aliyuncs.com') \
+            .replace("https://gitee.com/wmyskxz/pic/raw/master", 'https://wmyskxz-blog.oss-cn-chengdu.aliyuncs.com/pic/raw/master') \
+            .replace("https://files.mdnice.com/user/4774", 'https://wmyskxz-blog.oss-cn-chengdu.aliyuncs.com/gitee')
 
-    with open(fileName, 'w', encoding='utf-8') as dump_f:
-      dump_f.write(fileLines)
-    end = time.time()
-    print('\n========处理完成：', fileName, '， 耗时：', (end - start), 's')
+        with open(fileName, 'w', encoding='utf-8') as dump_f:
+            dump_f.write(fileLines)
+        end = time.time()
+        print('\n========处理完成：', fileName, '， 耗时：', (end - start), 's')
